1cedd3286cc8ffc5f5c00b3d20d4f35e3afce86e
Dahlia Ghonim <dahlia.ghonim@gmail.com>
2025-11-01 16:49:01 +0200

    Passenger counter app - fix dash after last count

--- week3/day1/index.js
+++ week3/day1/index.js
@@ -2,6 +2,7 @@
 let countEl = document.getElementById("count-el")
 let count = 0
 let saveEl = document.getElementById("save-el")
+let firstSave = true
 
 function increment() {
 	count += 1
@@ -9,7 +10,15 @@ function increment() {
 }
 
 function save() {
-	let countStr = count + " - "
+	let countStr = null
+	if (true === firstSave) {
+		countStr = count
+		firstSave = false
+	}
+	else {
+		countStr = " - " + count
+	}
+
 	saveEl.textContent += countStr
 	count = 0
 	countEl.textContent = count
